4702ac5b74bbdb8a21c0bdcc6553f5b1e54ec9ca
João Paulo <jpaulope@indracompany.com>
2021-02-12 19:34:17 -0300

    feat: event store

--- src/routes/events.js
+++ src/routes/events.js
@@ -3,5 +3,6 @@ const events = require('express').Router();
 const EventController = require('../controllers/Events/EventController.js');
 
 events.get('/', EventController.index);
+events.post('/', EventController.store);
 
 module.exports = events;
