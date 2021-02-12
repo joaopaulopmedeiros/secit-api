b33d8393adbef887b26c7b8170f17d9865ce6cba
diogo <diogo.libras43@gmail.com>
2021-02-12 19:33:47 -0300

    migracao

--- src/models/Event.js
+++ src/models/Event.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 const EventSchema = new mongoose.Schema({
     name: String,
     year: Number, 
+    prazo_submissao: new Date(),
+    inicio_apresentacao: new Date(),
+    fim_apresentacao: new Date(),
+    timestamps: { 
+        createdAt: 'created_at' 
+    }
 });
 
 module.exports = mongoose.model('Event', EventSchema);
