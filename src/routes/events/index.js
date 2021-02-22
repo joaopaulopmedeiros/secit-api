06b6fcca762b801564eff7f7af1746a22b8db1c5
diogo <diogo.libras43@gmail.com>
2021-02-22 20:54:44 -0300

    DELETE NA ROTA NÃO TROQUEM

--- src/routes/events/index.js
+++ src/routes/events/index.js
@@ -4,8 +4,9 @@ const EventController = require('../../controllers/EventController.js');
 
 events.get('/', EventController.index);
 events.post('/', EventController.store);
-events.delete('/', EventController.delete);
+events.delete('/delete', EventController.delete);
 events.get('/:id', EventController.show);
 events.put('/:id', EventController.update);
+
 
 module.exports = events;
