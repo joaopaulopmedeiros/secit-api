9de0bebd42aa62b569fe14757c988ee669fadf71
João Paulo <jpaulope@indracompany.com>
2021-03-07 20:47:41 -0300

    chore: removing mongodb warnings

--- src/utils/initializeDatabase.js
+++ src/utils/initializeDatabase.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const initializeDatabase = async () => {
     await mongoose.connect(`${process.env.MONGO_DB_CONNECTION}`, {
+        useCreateIndex: true,
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
