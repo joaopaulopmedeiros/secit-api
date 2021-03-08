630b6aa754ab1bc665446a2ad54db6bffc5c0973
João Paulo <jpaulope@indracompany.com>
2021-03-07 22:31:15 -0300

    refactor: removing unecessary code

--- src/database/migrations/20210219112805-events.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  async up(db, client) {
-    await db.collection('events').updateOne({name: 'SECIT'}, {$set: {blacklisted: true}});
-  },
-
-  async down(db, client) {
-    await db.collection('events').updateOne({name: 'SECIT'}, {$set: {blacklisted: false}});
-  }
-};
